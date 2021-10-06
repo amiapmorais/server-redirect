ca769891509d1947585a61dc802a567efafd033d
amiapmorais <amiaram91@gmail.com>
2021-10-06 07:35:22 -0300

    Update app.py
    adding / route to webhook check the endpoint

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import requests
 app = Flask(__name__)
+@app.route('/')
+def home():
+    return '', 200
 @app.route('/pix', methods=['POST'])
 def pix():
     requests.post('https://script.google.com/macros/s/AKfycbxsGXB18bwXhPoeQhRKJ2yre4ONITdXwHwjB1EBaLVcXilNTpUQechYfzO9nSRCDab0kg/exec', json= request.json)
