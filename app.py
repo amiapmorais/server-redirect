09b18c9fcce9fcf47a9140e8bee806bcb5f5d742
amiapmorais <amiaram91@gmail.com>
2021-10-07 18:23:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def home():
     return '', 200
 @app.route('/pix', methods=['POST'])
 def pix():
-    requests.post('https://script.google.com/macros/s/AKfycbynL14kinLVRiTaLBYuGFeWdhzAlzZKHzPZD62qXSXLGV860Oa9OHW_RYHU1uNe_9sh-A/exec', json= request.json)
+    requests.post('https://script.google.com/macros/s/AKfycbxpZj8yWx-WjVPEh-HSMiCwbij-dUS_s1dmKbfGPN-pQ88qXA6JYnkLKy27mDBVPEXgyg/exec', json= request.json)
     return '', 200
 """if __name__ == '__main__':
     app.run((debug=True)"""
